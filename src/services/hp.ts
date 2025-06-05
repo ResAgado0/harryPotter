862e59e3c1e73d67e9203471f86411c984ea48c9
Jesus <jesus.damaro35@gmail.com>
2025-06-05 15:48:47 +0200

    Mostrar todos los personajes además de la búsqueda

--- src/services/hp.ts
+++ src/services/hp.ts
@@ -1,20 +1,24 @@
 import { type HarryPotter } from "../types/personajes.ts"
 
+export async function getCharacters() {
+    const res = await fetch(`http://192.168.1.140/hyptt/personajesHarryPotter.json`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    )
+    const characters = await res.json() as HarryPotter[];
+    console.log(characters);
+    return characters;
+}
+
 export const getCharacterByName = async ({name} : {name: string}) => {
 
-    const res = await fetch(`http://192.168.1.140/hyptt/personajesHarryPotter.json`, 
-    {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    });
-    // const character = await res.json() as HarryPotter;
-    // console.log(character)
-    const characters = await res.json() as HarryPotter[];
-    const character = characters.find(c => c.name === name || c.alternate_names.includes(name));
+    const characters = await getCharacters();
+    const character = characters.find(c => c.name === name || c.alternate_names.includes(name) || c.house.includes(name));
     console.log(character);
 
-    return character
-
+    return character;
 }

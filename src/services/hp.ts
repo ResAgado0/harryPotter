b9bb7e847f6ae7644c7680dc368f1e0b0908a4e3
resagado0 <jesus.damaro35@gmail.com>
2026-01-21 12:03:56 +0100

    Change icon page and fix responsive

--- src/services/hp.ts
+++ src/services/hp.ts
@@ -1,36 +1,15 @@
 import { type HarryPotter } from "../types/personajes.ts";
 
 // Lógica para cliente (browser)
-export async function getCharactersClient() {
-    const res = await fetch(`/hyptt/personajesHarryPotter.json`, {
-        method: "GET",
+export async function getCharacters() {
+    const res = await fetch('https://harrypotter.resagado.dev/hyptt/personajes.json', {
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json",
-        }
+            'Content-Type': 'application/json',
+        },
     });
     const characters = await res.json() as HarryPotter[];
     return characters;
-}
-
-// Lógica para servidor (Node.js/Astro)
-export async function getCharactersServer() {
-    const { promises: fs } = await import("fs");
-    const path = await import("path");
-    const filePath = path.resolve(process.cwd(), "public/hyptt/personajesHarryPotter.json");
-    const data = await fs.readFile(filePath, "utf-8");
-    const characters = JSON.parse(data) as HarryPotter[];
-    return characters;
-}
-
-// Ejemplo de función que usa la lógica adecuada según el entorno
-export async function getCharacters() {
-    if (typeof window !== "undefined") {
-        // Cliente
-        return getCharactersClient();
-    } else {
-        // Servidor
-        return getCharactersServer();
-    }
 }
 
 export const getCharacterByName = async ({ name }: { name: string }) => {
